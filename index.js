e7d3dd5cb7ce3a5b8b2a8b7ac092936d1c9af3d6
Gergely Nemeth <gergo@secretsaucepartners.com>
2013-12-16 11:29:01 +0100

    path,fs fixed

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 /**
  * Make iodocs more module friendly by enabling config injection
  * */
+var fs = require('fs'),
+  path = require('path');
 
 module.exports = function (config) {
   if (!config) {
     console.error('Please provide a configuration object!');
     process.exit(-1);
   }
-  fs.writeFile('config.json', JSON.stringify(config), function (err) {
+  fs.writeFile(path.join(__dirname, 'config.json'), JSON.stringify(config), function (err) {
     if (err) throw err;
     return require('./app.js');
   });
